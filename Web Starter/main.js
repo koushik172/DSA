5fba845bef34ab2c62967de79a17ecf0da726fa3
koushik172 <koushikrajbanshi14@gmail.com>
2023-09-15 23:39:36 +0530

    Making changes using selectors

--- Web Starter/main.js
+++ Web Starter/main.js
@@ -1,6 +1,23 @@
 let items = document.getElementsByClassName('list-group-item');
+
+for (var i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = '#e8f8f8';
+}
+
 items[2].style.backgroundColor = "green";
 
-let list = document.getElementById("items");
-list.style.fontWeight = "bold";
-list.style.color = 'blue';
+let list = document.getElementById('items');
+
+var childElement = document.createElement("li");
+childElement.className = "childClass";
+
+list.appendChild(childElement);
+
+childElement = document.getElementsByClassName('childClass');
+childElement[0].textContent = "Item 5";
+
+childElement = document.getElementsByTagName("li")
+
+childElement[4].classList.add("list-group-item");
+childElement[4].style.backgroundColor = '#111';
+childElement[4].style.color = 'white';
