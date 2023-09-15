239a3158c9da077d266dc0455599be9a49278818
koushik172 <koushikrajbanshi14@gmail.com>
2023-09-15 15:25:23 +0530

    List Colour and Bold Font

---
+++ Web Starter/main.js
@@ -0,0 +1,6 @@
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = "green";
+
+let list = document.getElementById("items");
+list.style.fontWeight = "bold";
+list.style.color = 'blue';
